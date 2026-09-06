7eb03c9d9344ba9e5fa52e0f2f0134bd4228a337
Thiago-Cardoso <thiago.cardoso@abstra.co>
2026-09-06 11:12:43 -0300

    feat: add animals HTTP endpoint

--- src/animal_management/http.clj
+++ src/animal_management/http.clj
@@ -1,5 +1,6 @@
 (ns animal-management.http
-  (:require [reitit.ring :as ring]))
+  (:require [reitit.ring :as ring]
+            [cheshire.core :as json]))
 
 (defn health-handler
   [_request]
@@ -7,7 +8,19 @@
    :headers {"Content-Type" "text/plain"}
    :body "OK"})
 
+(def animals
+  [{:name "Luna"
+    :species :dog
+    :status :available}])
+
+(defn animals-handler
+  [_request]
+  {:status 200
+   :headers {"Content-Type" "application/json"}
+   :body (json/generate-string animals)})
+
 (def app
   (ring/ring-handler
     (ring/router
-      [["/health" {:get health-handler}]])))
+      [["/health" {:get health-handler}]
+       ["/animals" {:get animals-handler}]])))
--- test/animal_management/http_test.clj
+++ test/animal_management/http_test.clj
@@ -1,6 +1,7 @@
 (ns animal-management.http-test
   (:require [clojure.test :refer [deftest is]]
-            [animal-management.http :as http]))
+            [animal-management.http :as http]
+            [cheshire.core :as json]))
 
 (deftest health-handler-test
   (let [response (http/health-handler {})]
@@ -16,3 +17,21 @@
     (is (= "text/plain"
            (get-in response [:headers "Content-Type"])))
     (is (= "OK" (:body response)))))
+
+(deftest animals-route-test
+  (let [response (http/app
+                   {:request-method :get
+                    :uri "/animals"})]
+    (is (= 200 (:status response)))
+    (is (= "application/json"
+           (get-in response [:headers "Content-Type"])))
+    (is (= "[{\"name\":\"Luna\",\"species\":\"dog\",\"status\":\"available\"}]"
+           (:body response)))))
+
+(deftest json-generation-test
+  (let [animals [{:name "Luna"
+                  :species :dog
+                  :status :available}]
+        json-string (json/generate-string animals)]
+    (is (= "[{\"name\":\"Luna\",\"species\":\"dog\",\"status\":\"available\"}]"
+           json-string))))
